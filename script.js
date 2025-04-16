87a1e03176a95ca63ab649ca1a83016260bb64dc
ScuderiaCanziani <s-armarolli.christian@isiskeynes.it>
2025-04-16 19:05:57 +0200

    Rifatto tutto

--- script.js
+++
@@ -1,5 +0,0 @@
-// Gestisce l'apertura e la chiusura del menu laterale
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active'); // Aggiunge/lega la classe active per aprire/chiudere il menu
-});

203ad5192c6ac3f43759dadb1663cad95df72263
Christian Armarolli <christianarmarolli@gmail.com>
2025-04-17 00:12:00 +0200

    Projects added

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
         menuList.style.display = 'block'; // Mostra i link
     }
 });
+
+  function openModal(url) {
+    const modal = document.getElementById("canvaModal");
+    const frame = document.getElementById("canvaFrame");
+    frame.src = url;
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    const modal = document.getElementById("canvaModal");
+    const frame = document.getElementById("canvaFrame");
+    modal.style.display = "none";
+    frame.src = ""; // svuota per bloccare il video se presente
+  }
+
+  // Chiudi anche con ESC
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape") closeModal();
+  });
+
